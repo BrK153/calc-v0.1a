090e6b47b945c71657f981228331770a30a06183
BrK153 <125916672+BrK153@users.noreply.github.com>
2023-07-07 07:48:08 -0300

    Add files via upload

---
+++ calc_v0.1.1a.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+# add % #
+import tkinter as tk
+from tkinter import ttk
+
+# Função para atualizar o display com o número selecionado
+def atualizar_display(numero):
+    display.config(state=tk.NORMAL)
+    display.insert(tk.END, numero)
+    display.config(state=tk.DISABLED)
+
+# Função para exibir o resultado no display
+def exibir_resultado():
+    expressao = display.get(1.0, tk.END)
+    resultado = avaliar_expressao(expressao)
+    if resultado is not None:
+        display.config(state=tk.NORMAL)
+        display.delete(1.0, tk.END)
+        display.insert(tk.END, str(resultado))
+        display.config(state=tk.DISABLED)
+    else:
+        display.config(state=tk.NORMAL)
+        display.delete(1.0, tk.END)
+        display.insert(tk.END, "Erro")
+        display.config(state=tk.DISABLED)
+
+# Função para limpar o display
+def limpar_display():
+    display.config(state=tk.NORMAL)
+    display.delete(1.0, tk.END)
+    display.config(state=tk.DISABLED)
+
+# Função para excluir o último caractere
+def excluir_caractere():
+    display.config(state=tk.NORMAL)
+    display.delete('end-2c')  # Apaga o último caractere digitado
+    display.config(state=tk.DISABLED)
+
+# Função para lidar com a porcentagem
+def calcular_porcentagem():
+    expressao = display.get(1.0, tk.END)
+    try:
+        # Extrair o número e a porcentagem da expressão
+        numero, porcentagem = expressao.split('+')
+        numero = float(numero.strip())
+        porcentagem = float(porcentagem.strip().replace('%', ''))
+
+        # Calcular a porcentagem
+        resultado = numero + (numero * porcentagem / 100)
+
+        display.config(state=tk.NORMAL)
+        display.delete(1.0, tk.END)
+        display.insert(tk.END, str(resultado))
+        display.config(state=tk.DISABLED)
+    except (ValueError, ZeroDivisionError, IndexError):
+        display.config(state=tk.NORMAL)
+        display.delete(1.0, tk.END)
+        display.insert(tk.END, "Erro")
+        display.config(state=tk.DISABLED)
+
+# Função para avaliar a expressão matemática
+def avaliar_expressao(expressao):
+    try:
+        # Remove espaços em branco
+        expressao = expressao.replace(' ', '')
+        # Substitui os símbolos de operação por operadores matemáticos
+        expressao = expressao.replace('×', '*').replace('÷', '/')
+        # Avalia a expressão
+        resultado = eval(expressao)
+        return resultado
+    except (SyntaxError, TypeError, ZeroDivisionError, NameError):
+        return None
+
+# Cria a janela principal
+janela = tk.Tk()
+janela.title("Calculadora")
+janela.configure(background='#222222')  # Definindo a cor de fundo da janela
+
+# Cria o display
+display = tk.Text(janela, height=2, width=20, bd=3, relief=tk.SUNKEN)
+display.config(state=tk.DISABLED, bg='#222222', fg='white')  # Definindo a cor de fundo e cor do texto do display
+display.pack(pady=10)
+
+# Cria os botões
+botoes = [
+    ['AC', '⇦', '%'],
+    ['7', '8', '9', '/'],
+    ['4', '5', '6', '×'],
+    ['1', '2', '3', '-'],
+    ['0', '.', '+', '='],
+]
+
+for linha in botoes:
+    frame = tk.Frame(janela, bg='#222222')  # Definindo a cor de fundo do frame
+    frame.pack()
+    for valor in linha:
+        if valor == '=':
+            botao = ttk.Button(frame, text=valor, width=5, command=exibir_resultado, style='Equal.TButton')
+        elif valor == '/':
+            botao = ttk.Button(frame, text=valor, width=5, command=lambda x=valor: atualizar_display(x), style='Operator.TButton')
+        elif valor in ('×', '-', '+'):
+            botao = ttk.Button(frame, text=valor, width=5, command=lambda x=valor: atualizar_display(x), style='Operator.TButton')
+        elif valor == '⇦':
+            botao = ttk.Button(frame, text=valor, width=5, command=excluir_caractere, style='Delete.TButton')
+        elif valor == 'AC':
+            botao = ttk.Button(frame, text=valor, width=5, command=limpar_display, style='Clear.TButton')
+        elif valor == '%':
+            botao = ttk.Button(frame, text=valor, width=5, command=calcular_porcentagem, style='Operator.TButton')
+        else:
+            botao = ttk.Button(frame, text=valor, width=5, command=lambda x=valor: atualizar_display(x), style='Number.TButton')
+        botao.pack(side=tk.LEFT)
+
+# Cria o rótulo com o texto da borda final
+rodape_frame = tk.Frame(janela, bg='#222222')  # Definindo o frame para agrupar os rótulos
+rodape_frame.pack(side=tk.BOTTOM, pady=10)
+
+rodape = tk.Label(rodape_frame, text="(Criado por @joku.moba)", bg='#222222', fg='white', font=('Arial', 5, 'italic'))
+rodape.pack(side=tk.LEFT)
+
+versao = tk.Label(rodape_frame, text="calc v0.1a", bg='#222222', fg='white', font=('Arial', 2, 'italic'))
+versao.pack(side=tk.LEFT, padx=10)
+
+# Estilo dos botões
+style = ttk.Style()
+style.configure('Equal.TButton', font=('Arial', 14, 'bold'), background='#4CAF50', foreground='white')
+style.configure('Operator.TButton', font=('Arial', 14, 'bold'), background='#FF5722', foreground='white')
+style.configure('Number.TButton', font=('Arial', 14, 'bold'), background='#FFFFFF', foreground='#222222')
+style.configure('Delete.TButton', font=('Arial', 14, 'bold'), background='#F44336', foreground='white')
+style.configure('Clear.TButton', font=('Arial', 14, 'bold'), background='#FF9800', foreground='white')
+
+# Inicia o loop da janela
+janela.mainloop()
